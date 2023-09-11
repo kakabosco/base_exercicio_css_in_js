73eed368e7bd9503fdd9e3dc777d69925e0c5b0f
Kaique Bosco <kaique.bosco@hotmail.com>
2023-09-11 10:54:17 -0300

    styled components applied

--- src/containers/ListaVagas/index.tsx
+++ src/containers/ListaVagas/index.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import FormVagas from '../../components/FormVagas'
+import { useState } from "react"
+import FormVagas from "../../components/FormVagas"
 
-import Vaga from '../../components/Vaga'
+import Vaga from "../../components/Vaga"
 
-import styles from './ListaVagas.module.css'
+import { Vagas } from "./styles"
 
 type Vaga = {
   id: string
@@ -19,78 +19,78 @@ type Vaga = {
 const vagas = [
   {
     id: 1,
-    titulo: 'Desenvolvedor front-end',
-    localizacao: 'remoto',
-    nivel: 'junior',
-    modalidade: 'clt',
+    titulo: "Desenvolvedor front-end",
+    localizacao: "remoto",
+    nivel: "junior",
+    modalidade: "clt",
     salarioMin: 3000,
     salarioMax: 4500,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   },
   {
     id: 2,
-    titulo: 'Desenvolvedor NodeJS',
-    localizacao: 'remoto',
-    nivel: 'pleno',
-    modalidade: 'pj',
+    titulo: "Desenvolvedor NodeJS",
+    localizacao: "remoto",
+    nivel: "pleno",
+    modalidade: "pj",
     salarioMin: 5000,
     salarioMax: 6500,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   },
   {
     id: 3,
-    titulo: 'Desenvolvedor fullstack',
-    localizacao: 'remoto',
-    nivel: 'pleno',
-    modalidade: 'pj',
+    titulo: "Desenvolvedor fullstack",
+    localizacao: "remoto",
+    nivel: "pleno",
+    modalidade: "pj",
     salarioMin: 4000,
     salarioMax: 6000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   },
   {
     id: 4,
-    titulo: 'Designer de interfaces',
-    localizacao: 'remoto',
-    nivel: 'junior',
-    modalidade: 'clt',
+    titulo: "Designer de interfaces",
+    localizacao: "remoto",
+    nivel: "junior",
+    modalidade: "clt",
     salarioMin: 4000,
     salarioMax: 5000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   },
   {
     id: 5,
-    titulo: 'Desenvolvedor front-end',
-    localizacao: 'remoto',
-    nivel: 'senior',
-    modalidade: 'clt',
+    titulo: "Desenvolvedor front-end",
+    localizacao: "remoto",
+    nivel: "senior",
+    modalidade: "clt",
     salarioMin: 7000,
     salarioMax: 8000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   },
   {
     id: 6,
-    titulo: 'Desenvolvedor front-end para projeto internacional',
-    localizacao: 'remoto',
-    nivel: 'senior',
-    modalidade: 'pj',
+    titulo: "Desenvolvedor front-end para projeto internacional",
+    localizacao: "remoto",
+    nivel: "senior",
+    modalidade: "pj",
     salarioMin: 12000,
     salarioMax: 15000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   },
   {
     id: 7,
-    titulo: 'Desenvolvedor front-end',
-    localizacao: 'São Paulo/SP',
-    nivel: 'junior',
-    modalidade: 'clt',
+    titulo: "Desenvolvedor front-end",
+    localizacao: "São Paulo/SP",
+    nivel: "junior",
+    modalidade: "clt",
     salarioMin: 4000,
     salarioMax: 5000,
-    requisitos: ['HTML', 'CSS', 'JavaScript', 'jQuery']
+    requisitos: ["HTML", "CSS", "JavaScript", "jQuery"]
   }
 ]
 
 const ListaVagas = () => {
-  const [filtro, setFiltro] = useState<string>('')
+  const [filtro, setFiltro] = useState<string>("")
 
   const vagasFiltradas = vagas.filter(
     (x) => x.titulo.toLocaleLowerCase().search(filtro) >= 0
@@ -99,7 +99,7 @@ const ListaVagas = () => {
   return (
     <div>
       <FormVagas aoPesquisar={(termo: string) => setFiltro(termo)} />
-      <ul className={styles.vagas}>
+      <Vagas>
         {vagasFiltradas.map((vag) => (
           <Vaga
             key={vag.id}
@@ -112,7 +112,7 @@ const ListaVagas = () => {
             requisitos={vag.requisitos}
           />
         ))}
-      </ul>
+      </Vagas>
     </div>
   )
 }
